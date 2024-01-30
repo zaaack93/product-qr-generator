fc08e9a7bea48f5898563140b8f9ab2105f751f5
b.zakaria <zakaria_bouzangad@gmail.Com>
2024-01-30 11:57:59 +0100

    add other files

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React from "react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
 
-type Props = {}
+import { authenticate } from "~/shopify.server";
+import { getQRCodes } from "~/models/QRcode.server";
+import { Link, useLoaderData,useNavigate } from "@remix-run/react";
+import { Card, EmptyState, Icon, IndexTable, InlineStack, Layout, Page, Text, Thumbnail } from "@shopify/polaris";
+import { ImageMajor } from "@shopify/polaris-icons";
 
-export default function Index({}: Props) {
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { admin, session } = await authenticate.admin(request);
+  const qrCodes = await getQRCodes(session.shop, admin.graphql);
+
+  return json({
+    qrCodes,
+  });
+}
+
+interface EmptyQRCodeStateProps {
+  onAction: () => void;
+}
+
+const EmptyQRCodeState: React.FC<EmptyQRCodeStateProps> = ({ onAction }) => (
+  <EmptyState
+    heading="Create unique QR codes for your product"
+    action={{
+      content: "Create QR code",
+      onAction,
+    }}
+    image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+  >
+    <p>Allow customers to scan codes and buy products using their phones.</p>
+  </EmptyState>
+);
+
+function truncate(str, { length = 25 } = {}) {
+  if (!str) return "";
+  if (str.length <= length) return str;
+  return str.slice(0, length) + "…";
+}
+
+const QRTable = ({ qrCodes }) => (
+  <IndexTable
+    resourceName={{
+      singular: "QR code",
+      plural: "QR codes",
+    }}
+    itemCount={qrCodes.length}
+    headings={[
+      { title: "Thumbnail", hidden: true },
+      { title: "Title" },
+      { title: "Product" },
+      { title: "Date created" },
+      { title: "Scans" },
+    ]}
+    selectable={false}
+  >
+    {qrCodes.map((qrCode) => (
+      <QRTableRow key={qrCode.id} qrCode={qrCode} />
+    ))}
+  </IndexTable>
+);
+
+const QRTableRow = ({ qrCode }) => (
+  <IndexTable.Row id={qrCode.id} position={qrCode.id}>
+    <IndexTable.Cell>
+      <Thumbnail
+        source={qrCode.productImage || ImageMajor}
+        alt={qrCode.productTitle}
+        size="small"
+      />
+    </IndexTable.Cell>
+    <IndexTable.Cell>
+      <Link to={`qrcodes/${qrCode.id}`}>{truncate(qrCode.title)}</Link>
+    </IndexTable.Cell>
+    <IndexTable.Cell>
+      {qrCode.productDeleted ? (
+        <InlineStack align="start" gap="200">
+          <span style={{ width: "20px" }}>
+            <Icon source={DiamondAlertMajor} tone="critical" />
+          </span>
+          <Text tone="critical" as="span">
+            product has been deleted
+          </Text>
+        </InlineStack>
+      ) : (
+        truncate(qrCode.productTitle)
+      )}
+    </IndexTable.Cell>
+    <IndexTable.Cell>
+      {new Date(qrCode.createdAt).toDateString()}
+    </IndexTable.Cell>
+    <IndexTable.Cell>{qrCode.scans}</IndexTable.Cell>
+  </IndexTable.Row>
+);
+
+export default function Index() {
+  const { qrCodes } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+
+
   return (
-    <div>app._index</div>
-  )
+    <Page>
+      <ui-title-bar title="QR codes">
+        <button variant="primary" onClick={() => navigate("/app/qrcodes/new")}>
+          Create QR code
+        </button>
+      </ui-title-bar>
+      <Layout>
+        <Layout.Section>
+          <Card padding="0">
+            {qrCodes.length === 0 ? (
+              <EmptyQRCodeState onAction={() => navigate("qrcodes/new")} />
+            ) : (
+              <QRTable qrCodes={qrCodes} />
+            )}
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
 }
